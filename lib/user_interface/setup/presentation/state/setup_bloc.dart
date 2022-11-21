62d23a877c7b8384ddc0be40c0c0cd9dd91d1fe7
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2022-11-21 12:23:38 +0530

    minor bugfix

--- lib/user_interface/setup/presentation/state/setup_bloc.dart
+++ lib/user_interface/setup/presentation/state/setup_bloc.dart
@@ -55,7 +55,7 @@ class SetupBloc extends TerminalBaseBloc<SetupEvent, SetupState> {
     bool isConnected=await _homeRepo.checkInternetAccess();
 
     navigate() async {
-      if (await _homeRepo.compatibilityChecker()==1) {
+      if (await _homeRepo.compatibilityChecker()==0) {
         emit(SetupCompatibleState());
       } else {
         if(isConnected) {
