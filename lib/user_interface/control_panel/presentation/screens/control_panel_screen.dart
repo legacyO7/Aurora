348fe0ba4e0e89e46c536c3be54d59aab3ea7833
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2023-02-02 13:48:12 +0530

    update ui

--- lib/user_interface/control_panel/presentation/screens/control_panel_screen.dart
+++ lib/user_interface/control_panel/presentation/screens/control_panel_screen.dart
@@ -1,9 +1,11 @@
 
 import 'package:aurora/user_interface/control_panel/presentation/screens/control_panel_widgets.dart';
 import 'package:aurora/user_interface/control_panel/presentation/screens/widgets/theme_button.dart';
+import 'package:aurora/user_interface/control_panel/state/keyboard_settings_bloc.dart';
 import 'package:aurora/utility/constants.dart';
 import 'package:aurora/utility/global_configuration.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ControlPanelScreen extends StatefulWidget {
   const ControlPanelScreen({Key? key}) : super(key: key);
@@ -27,10 +29,24 @@ class _ControlPanelState extends State<ControlPanelScreen> {
           children:  <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children:const [
-                ThemeButton(),
-                GitButton(),
-                UninstallButton()
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: context.read<KeyboardSettingsBloc>().selectedColor),
+                      borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20),topLeft: Radius.circular(20))
+                    ),
+                    child: const ThemeButton()),
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.symmetric(vertical: BorderSide.none,horizontal: BorderSide(color:context.watch<KeyboardSettingsBloc>().selectedColor ))
+                    ),
+                    child: const GitButton()),
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: context.watch<KeyboardSettingsBloc>().selectedColor),
+                        borderRadius: const BorderRadius.only(bottomRight: Radius.circular(20),topRight: Radius.circular(20))
+                    ),
+                    child: const UninstallButton())
               ],
             ),
             if(Constants.globalConfig.arMode!=ARMODE.faustus)
