aef4bf9aeff43f4273145bc8f7a7e6f0079bc960
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2023-06-29 17:04:20 +0530

    option to check for a file or directory

--- lib/data/io/io_manager/io_manager_impl.dart
+++ lib/data/io/io_manager/io_manager_impl.dart
@@ -6,39 +6,44 @@ import 'io_manager.dart';
 class IOManagerImpl implements IOManager{
 
   @override
+  Future<bool> checkIfExists({required dynamic filePath, required FileSystemEntityType fileType}) async{
+    File fileInQuestion=_convertPathToFile(filePath);
+    return (await fileInQuestion.exists() && (await fileInQuestion.stat()).type==fileType);
+  }
+
+  @override
   Future writeToFile({required dynamic filePath, required String content, FileMode fileMode=FileMode.write}) async{
     try{
-      if(filePath is String) {
-        await File(filePath).writeAsString(content,mode: fileMode);
-      }
-      if(filePath is File) {
-        await filePath.writeAsString(content,mode: fileMode);
-      }
+      await _convertPathToFile(filePath).writeAsString(content,mode: fileMode);
     }
     catch(_){}
   }
 
   @override
   Future<List<String>> readFile(dynamic filePath) async{
-
     try{
-      if(filePath is String) {
-        return await File(filePath).readAsLines();
-      }
-      if(filePath is File) {
-        return await filePath.readAsLines();
-      }
+        return await _convertPathToFile(filePath).readAsLines();
     }
     catch(e){
       return [];
     }
-
-    return [];
   }
 
   @override
   Future<String> getFileStat(String filePath) async{
     return (await File(filePath).stat()).modeString();
   }
+
+  File _convertPathToFile(dynamic filePath){
+    if(filePath is String) {
+      return File(filePath);
+    }
+    if(filePath is File) {
+      return filePath;
+    }
+
+    throw ("I donno what that is");
+  }
+
 
 }
