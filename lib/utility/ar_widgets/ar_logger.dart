af3d24db8621cf74fb0aadbbff4edee15a1c4c24
legacyO7 <46623012+legacyO7@users.noreply.github.com>
2023-07-06 18:53:49 +0530

    arLogger is singleton
    set arLogger as singleton and detach it from getit to remove circular dependency

--- lib/utility/ar_widgets/ar_logger.dart
+++ lib/utility/ar_widgets/ar_logger.dart
@@ -1,28 +1,44 @@
 import 'dart:io';
 
+import 'package:aurora/data/init_aurora.dart';
 import 'package:aurora/data/io/io_manager/io_manager.dart';
 import 'package:aurora/utility/constants.dart';
 import 'package:aurora/utility/global_mixin.dart';
 import 'package:flutter/foundation.dart';
 
+
 class ArLogger with GlobalMixin{
 
-  ArLogger(this._ioManager);
+  static ArLogger? _instance;
+
+  ArLogger._();
+
+  factory ArLogger(){
+    _instance??=ArLogger._();
+    return _instance!;
+  }
 
   late File _logFile;
-  final IOManager _ioManager;
+  final IOManager _ioManager=sl<IOManager>();
   
   final List<String> _log=[];
   bool _logging=false;
 
+  static T arTry<T>(T Function() tryBlock) {
+    try {
+      return tryBlock();
+    } catch (error, stackTrace) {
+      _instance!.log(data: error,stackTrace: stackTrace);
+    }
+    throw ('error');
+  }
 
-
-  void log({required dynamic data, StackTrace? stackTrace}) {
+  Future log({required dynamic data, StackTrace? stackTrace})async {
     if(canLog()) {
       _log.add("$data\n ${stackTrace ?? ''}");
       if (!_logging) {
         _logging = true;
-        _logBuffer(data: _log.first);
+        await _logBuffer(data: _log.first);
       }
     }
   }
